905294a9927e273bcfdca10596d63f25da14cc39
Gilles Diacon <gilles.diacon@gmail.com>
2015-05-23 20:58:49 +0200

    gdi REST api is not generic

--- backend/api/index.php
+++ backend/api/index.php
@@ -4,8 +4,12 @@ use TcBern\Model\Info;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+// list of authorized entities
+$authorizedEntities = array("infos");
+
 $app = new Slim\Slim();
 
+// handle GET requests for /
 $app->get(
     '/',
     function () use ($app) {
@@ -13,128 +17,177 @@ $app->get(
     }
 )->name("home");
 
-
-// handle GET requests for /infos
+// handle GET requests for /entity
 $app->get(
-    '/infos',
-    function () use ($app) {
-        // query database for all infos
-        $infos = Info::all();
+    '/:entity',
+    function ($entity) use ($app) {
 
-        // send response header for JSON content type
-        $app->response()->header('Content-Type', 'application/json');
+        if (verifyEntityParam($entity)) {
 
-        // return JSON-encoded response body with query results
-        echo $infos->toJson();
-    }
-);
-
-// handle GET requests for /infos/:id
-$app->get(
-    '/infos/:id',
-    function ($id) use ($app) {
-        try {
-            // query database for one info by id
-            $info = Info::find($id);
+            // query database for all infos
+            $infos = Info::all();
 
             // send response header for JSON content type
             $app->response()->header('Content-Type', 'application/json');
 
             // return JSON-encoded response body with query results
-            if ($info) {
-                echo $info->toJson();
-            } else {
-                $app->response()->status(404);
+            echo $infos->toJson();
+        } else {
+            $app->response()->status(404);
+        }
+    }
+);
+
+// handle GET requests for /infos/:id
+$app->get(
+    '/:entity/:id',
+    function ($entity, $id) use ($app) {
+
+        // send response header for JSON content type
+        $app->response()->header('Content-Type', 'application/json');
+
+        if (verifyEntityParam($entity)) {
+
+            try {
+                // query database for one info by id
+                $info = Info::find($id);
+
+                // return JSON-encoded response body with query results
+                if ($info) {
+                    echo $info->toJson();
+                } else {
+                    $app->response()->status(404);
+                }
+            } catch (Exception $e) {
+                $app->response()->status(400);
+                $app->response()->header('X-Status-Reason', $e->getMessage());
             }
-        } catch (Exception $e) {
-            $app->response()->status(400);
-            $app->response()->header('X-Status-Reason', $e->getMessage());
+        } else {
+            $app->response()->status(404);
+            $app->response()->header('X-Status-Reason', "Entity \'$entity\' not found");
         }
     }
 );
 
 // handle POST requests to /articles
 $app->post(
-    '/infos',
-    function () use ($app) {
-        try {
-            // get and decode JSON request body
-            $request = $app->request();
-            $body = $request->getBody();
-            $input = json_decode($body);
+    '/:entity',
+    function ($entity) use ($app) {
 
-            // store article record
-            $info = new Info();
-            $info->title = (string)$input->title;
-            $info->content = (string)$input->content;
-            $info->save();
+        // return JSON-encoded response body
+        $app->response()->header('Content-Type', 'application/json');
 
-            // return JSON-encoded response body
-            $app->response()->header('Content-Type', 'application/json');
-            echo $info->toJson();
-        } catch (Exception $e) {
-            $app->response()->status(400);
-            $app->response()->header('X-Status-Reason', $e->getMessage());
+        if (verifyEntityParam($entity)) {
+
+            try {
+                // get and decode JSON request body
+                $request = $app->request();
+                $body = $request->getBody();
+                $input = json_decode($body);
+
+                // store article record
+                $info = new Info();
+                $info->title = (string)$input->title;
+                $info->content = (string)$input->content;
+                $info->save();
+
+                echo $info->toJson();
+            } catch (Exception $e) {
+                $app->response()->status(400);
+                $app->response()->header('X-Status-Reason', $e->getMessage());
+            }
+        } else {
+            $app->response()->status(404);
+            $app->response()->header('X-Status-Reason', "Entity \'$entity\' not found");
         }
     }
 );
 
 // handle POST requests to /articles/:id
 $app->post(
-    '/infos/:id',
-    function ($id) use ($app) {
-        try {
-            // get and decode JSON request body
-            $request = $app->request();
-            $body = $request->getBody();
-            $input = json_decode($body);
+    '/:entity/:id',
+    function ($entity, $id) use ($app) {
 
-            // query database for one info by id
-            $info = Info::find($id);
+        // return JSON-encoded response body
+        $app->response()->header('Content-Type', 'application/json');
 
-            // store modified article
-            // return JSON-encoded response body
-            if ($info) {
-                $info->title = (string)$input->title;
-                $info->content = (string)$input->content;
-                $info->save();
+        if (verifyEntityParam($entity)) {
 
+            try {
+                // get and decode JSON request body
+                $request = $app->request();
+                $body = $request->getBody();
+                $input = json_decode($body);
+
+                // query database for one info by id
+                $info = Info::find($id);
+
+                // store modified article
                 // return JSON-encoded response body
-                $app->response()->header('Content-Type', 'application/json');
-                echo $info->toJson();
-            } else {
-                $app->response()->status(404);
+                if ($info) {
+                    $info->title = (string)$input->title;
+                    $info->content = (string)$input->content;
+                    $info->save();
+
+                    echo $info->toJson();
+                } else {
+                    $app->response()->status(404);
+                }
+            } catch (Exception $e) {
+                $app->response()->status(400);
+                $app->response()->header('X-Status-Reason', $e->getMessage());
             }
-        } catch (Exception $e) {
-            $app->response()->status(400);
-            $app->response()->header('X-Status-Reason', $e->getMessage());
+        } else {
+            $app->response()->status(404);
+            $app->response()->header('X-Status-Reason', "Entity \'$entity\' not found");
         }
     }
 );
 
 // handle DELETE requests for /infos/:id
 $app->delete(
-    '/infos/:id',
-    function ($id) use ($app) {
-        try {
-            // query database for one info by id
-            $info = Info::find($id);
+    '/:entity/:id',
+    function ($entity, $id) use ($app) {
 
-            // send response header for JSON content type
-            $app->response()->header('Content-Type', 'application/json');
+        // return JSON-encoded response body
+        $app->response()->header('Content-Type', 'application/json');
 
-            // return JSON-encoded response body with query results
-            if ($info) {
-                $info->delete();
-                $app->response()->status(200);
-            } else {
-                $app->response()->status(404);
+        if (verifyEntityParam($entity)) {
+
+            try {
+                // query database for one info by id
+                $info = Info::find($id);
+
+                // return JSON-encoded response body with query results
+                if ($info) {
+                    $info->delete();
+                    $app->response()->status(200);
+                } else {
+                    $app->response()->status(404);
+                }
+            } catch (Exception $e) {
+                $app->response()->status(400);
+                $app->response()->header('X-Status-Reason', $e->getMessage());
             }
-        } catch (Exception $e) {
-            $app->response()->status(400);
-            $app->response()->header('X-Status-Reason', $e->getMessage());
+        } else {
+            $app->response()->status(404);
+            $app->response()->header('X-Status-Reason', "Entity \'$entity\' not found");
         }
     }
 );
 
 $app->run();
+
+/**
+ * Returns true if the entity parameter is part of
+ * the listed of authorized entities
+ *
+ * @param $entity string the entity param
+ * @return bool true if the entity param is authorized
+ */
+function verifyEntityParam($entity)
+{
+    global $authorizedEntities;
+
+    return in_array($entity, $authorizedEntities);
+}
