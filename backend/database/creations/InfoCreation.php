5d23d79598935659d587e26d77169a2fc9919fe8
Cedric Lavanchy <cedric.lavanchy.pro@gmail.com>
2015-09-20 20:07:16 +0200

    cla - improve the info table and add the infos from current site in the seed file

--- backend/database/creations/InfoCreation.php
+++ backend/database/creations/InfoCreation.php
@@ -12,7 +12,8 @@ class InfoCreation {
         Capsule::schema()->create('info', function($table) {
             $table->increments('id');
             $table->string('title');
-            $table->string('content');
+            $table->string('content', 5000);
+            $table->date('date');
 
             // automatically create 'created_at' and 'updated_at' columns
             $table->timestamps();
