f446ff390efb4e4fdbcf22dc0cda900b2652ee48
u213429 <cedric.lavanchy@sbb.ch>
2016-02-02 13:40:45 +0100

    cla - complete integration of gulp. Problem with angular seen but must be tested with the help of wamp

--- webapp/gulpfile.js
+++ webapp/gulpfile.js
@@ -3,17 +3,21 @@
 
 var gulp = require('gulp');
 var $$ = require('gulp-load-plugins')({lazy: true});
+var eventStream = require('event-stream');
 var del = require('del');
 var path = require('path');
 var runSequence = require('run-sequence');
 var useref = require('gulp-useref');
 var gulpif = require('gulp-if');
-var uglify = require('gulp-uglify');
+var inject = require('gulp-inject');
 var uglify = require('gulp-uglify');
 var cssnano = require('gulp-cssnano');
+var gulpFilter = require('gulp-filter');
+var concat = require('gulp-concat');
+var html2js = require('gulp-html2js');
 
 gulp.task('clean', function () {
-    del(['dist/*']);
+    del(['dist/**', 'temp/**']);
 });
 
 gulp.task('analyze', function () {
@@ -22,19 +26,38 @@ gulp.task('analyze', function () {
 
     return gulp.src([basePath + '/**/*.js', '!./Content/bower_components/**/*'])
         .pipe($$.jshint())
-        .pipe($$.jshint.reporter('jshint-stylish', {verbose: true}))
+        .pipe($$.jshint.reporter('jshint-stylish', { verbose: true }))
         .pipe($$.jshint.reporter('fail'));
 });
 
-gulp.task('release', ['clean'], function(){
-    return gulp.src('./app/*.html')
-        .pipe(useref())
-        .pipe(gulpif('*.js', uglify()))
-        .pipe(gulpif('*.css', cssnano()))
+gulp.task('release', ['clean'], function() {
+    gulp.src('app/images/*.png')
+        .pipe(gulp.dest('dist/images'));
+    
+    var partials = gulp.src(['app/**/*.html', '!app/index.html', '!app/bower_components/**/*.html'])
+        .pipe($$.htmlhint({'doctype-first': false}))
+        .pipe($$.htmlhint.reporter())
+        .pipe($$.htmlmin({ removeComments: true }))
+        .pipe(html2js({
+            outputModuleName: 'tcbernApp',
+            useStrict: true,
+            target: 'js'
+        }))
+        .pipe($$.concat({path: 'template.js', cwd: ''}))
+        .pipe($$.rev())
+        .pipe(gulp.dest('dist'));
+    
+    var main = gulp.src('app/*.html')
+        .pipe($$.useref())
+        .pipe(gulpif('*.js', $$.uglify()))
         .pipe(gulpif('*.js', $$.rev()))
+        .pipe(gulpif('*.css', $$.cssnano()))
         .pipe(gulpif('*.css', $$.rev()))
         .pipe($$.revReplace())
-        .pipe(gulpif('*.html', $$.htmlmin({removeComments: true})))
+        .pipe(inject(partials, { ignorePath: ['dist/'], addRootSlash: false, starttag: '<!-- inject:template-js -->' }))
+        .pipe(gulpif('*.html', $$.htmlmin({ removeComments: true })));
+    
+    return eventStream.merge(partials, main)
         .pipe($$.debug({title: 'Processed output File: '}))
         .pipe(gulp.dest('dist'));
 });
