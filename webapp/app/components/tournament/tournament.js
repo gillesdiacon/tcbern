6647c0796a064313824dab9226999be545641d21
Cedric Lavanchy <cedric.lavanchy.pro@gmail.com>
2018-08-28 14:40:40 +0200

    extract the edition template into a component

--- webapp/app/components/tournament/tournament.js
+++ webapp/app/components/tournament/tournament.js
@@ -10,27 +10,6 @@
 
     function TournamentController($authentication, $loadedcontent) {
         var vm = this;
-        vm.edit = false;
-        vm.canNotEdit = !$authentication.isAuthenticated || !$authentication.isInGroup('admin');
-        vm.email = 'turnier' + '@' + 'tcbern.ch';
-        vm.pageContent = '';
-
-        $loadedcontent.contentForKey('TOURNAMENT').then(function(response) {
-            vm.content = response;
-            vm.pageContent = vm.content.content;
-        });
-
-        vm.startEdit = function() {
-            vm.edit = true;
-        }
-        vm.save = function() {
-            vm.content.content = vm.pageContent;
-            vm.content.put();
-            vm.edit = false;
-        }
-        vm.cancelEdit = function() {
-            vm.pageContent = vm.content.content;
-            vm.edit = false;
-        }
+        vm.tournamentKey = 'TOURNAMENT';
     }
 })();
---
+++ webapp/app/shared/editable/editable.js
@@ -0,0 +1,38 @@
+(function() {
+    'use strict';
+
+    var tcbernControllers = angular.module('tcbernControllers');
+    tcbernControllers.component('appEditable', {
+        templateUrl: 'shared/editable/editable.html',
+        controllerAs: 'vm',
+        controller: ['$scope', '$authentication', '$loadedcontent', EditableController],
+        bindings: {
+            contentKey: '='
+        }
+    });
+
+    function EditableController($scope, $authentication, $loadedcontent) {
+        var vm = this;
+        vm.edit = false;
+        vm.canNotEdit = !$authentication.isAuthenticated || !$authentication.isInGroup('admin');
+        vm.pageContent = '';
+
+        $loadedcontent.contentForKey(vm.contentKey).then(function(response) {
+            vm.content = response;
+            vm.pageContent = vm.content.content;
+        });
+
+        vm.startEdit = function() {
+            vm.edit = true;
+        }
+        vm.save = function() {
+            vm.content.content = vm.pageContent;
+            vm.content.put();
+            vm.edit = false;
+        }
+        vm.cancelEdit = function() {
+            vm.pageContent = vm.content.content;
+            vm.edit = false;
+        }
+    }
+})();
